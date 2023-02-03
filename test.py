2c3ef76fa540fba5fb56bff86a790307c545088a
dimaezi <wind.crazy125@gmail.com>
2023-02-03 17:31:41 +0300

    push

---
+++ test.py
@@ -0,0 +1,14 @@
+from MondayScriptTimes import CreatSheduleTimes
+import datetime
+
+
+
+
+# CreatSheduleTimes.monday()
+
+def article():
+    id = int(str(datetime.datetime.today().isoweekday())+str(datetime.datetime.today().strftime('%H%M')))
+    print(id)
+
+id = article() 
+

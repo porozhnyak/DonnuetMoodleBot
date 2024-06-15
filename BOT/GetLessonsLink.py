055c41ed99675238642d9517fa6d05a55bab15ee
Dmitry <wind.crazy125@gmail.com>
2024-06-15 15:44:33 +0300

    deleted comment code

--- BOT/GetLessonsLink.py
+++ BOT/GetLessonsLink.py
@@ -12,9 +12,3 @@ async def GetLessonsLink(today, TimeNow):
         return LessonsLink.Lessons_lib[today][TimeNow]
     except KeyError:
         pass
-# while True:
-#     today = str(datetime.datetime.today().isoweekday())# Выводит номер дня недели (1-Понедельник ... 7-Воскресенье)
-#     TimeNow = datetime.datetime.today().strftime('%H:%M')
-
-#     asyncio.run(GetLessonsLink(today, TimeNow))
-#     time.sleep(10)
